8e78e53358a57dc9e75950cfb833f3e242bd7526
geunheejung <geuni1013@gmail.com>
2019-01-15 00:59:33 +0900

    3강 class 기반 view CLEAR

--- snippets/views.py
+++ snippets/views.py
@@ -1,57 +1,85 @@
-from django.http import HttpResponse
-from rest_framework.parsers import JSONParser
 from .models import Snippet
 from .serializers import SnippetSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
-'''
-특정 콘텐츠 형태에 대한 요청이나 응답을 명시적으로 연결하지 않음.
-request.data는 JSON 요청 뿐만 아니라 yaml과 같은 다른 포맷도 다룰 수 있다.
-REST 프레임워크에서는 우리가 원하는 형태로 응답 객체를 렌더링해준다.
-'''
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
+
+
+# HTTP 메서드를 분리함.
+# class SnippetList(APIView):
+#     """
+#     코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
+#     """
+#     def get(self, request, format=None):
+#         snippets = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# 👇 믹스인 + 제네릭뷰
+# class SnippetList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#
+#     # *args는 복수의 파라미터를 튜플 형태로 받는다.
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#     # **kwargs 파라미터 명을 같이 보낼 수 있다. kwargs는 딕셔너리 형태로 전달된다.
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+# class SnippetDetail(APIView):
+#     """
+#     코드 조각 조회, 업데이트, 삭제
+#     """
+#
+#     def get_object(self, pk):
+#         try:
+#             return Snippet.objects.get(pk=pk)
+#         except Snippet.DoesNotExist:
+#             return Http404
+#
+#     def get(self, request, pk, format=None):
+#         try:
+#             snippet = self.get_object(pk)
+#             serializer = SnippetSerializer(snippet)
+#             return Response(serializer.data)
+#         except:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     def put(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         snippet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# 👇 제네릭 클래스 기반 뷰
+class SnippetList(generics.ListCreateAPIView):
     """
     코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
     """
-    if request.method == 'GET':
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
-    """
-    코드 조각 조회, 업데이트, 삭제
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
-
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
