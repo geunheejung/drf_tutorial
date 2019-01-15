70f91b4bb168808a5f10beb50e1b032f33c82135
geunheejung <geuni1013@gmail.com>
2019-01-15 23:32:39 +0900

    4강 DRF SessionAuthentication, BasicAuthentication 인증 클리어

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,30 @@
 from django.forms import widgets
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    # source 인자로는 특정 필드를 지정 가능. 직렬화된 인스턴스의 속성 뿐만 아니라 마침표 표기 방식을 통해 특정 속성 탐색도 가능
+    # ReadOnlyField는 직렬화에 사용되었을 땐 언제나 읽기 전용
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = (
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+            'owner'
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- snippets/views.py
+++ snippets/views.py
@@ -1,75 +1,9 @@
 from .models import Snippet
-from .serializers import SnippetSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework import mixins
-from rest_framework import generics
+from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
 
-
-# HTTP 메서드를 분리함.
-# class SnippetList(APIView):
-#     """
-#     코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
-#     """
-#     def get(self, request, format=None):
-#         snippets = Snippet.objects.all()
-#         serializer = SnippetSerializer(snippets, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request, format=None):
-#         serializer = SnippetSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# 👇 믹스인 + 제네릭뷰
-# class SnippetList(mixins.ListModelMixin,
-#                   mixins.CreateModelMixin,
-#                   generics.GenericAPIView):
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-#
-#     # *args는 복수의 파라미터를 튜플 형태로 받는다.
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-#     # **kwargs 파라미터 명을 같이 보낼 수 있다. kwargs는 딕셔너리 형태로 전달된다.
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
-
-# class SnippetDetail(APIView):
-#     """
-#     코드 조각 조회, 업데이트, 삭제
-#     """
-#
-#     def get_object(self, pk):
-#         try:
-#             return Snippet.objects.get(pk=pk)
-#         except Snippet.DoesNotExist:
-#             return Http404
-#
-#     def get(self, request, pk, format=None):
-#         try:
-#             snippet = self.get_object(pk)
-#             serializer = SnippetSerializer(snippet)
-#             return Response(serializer.data)
-#         except:
-#             return Response(status=status.HTTP_404_NOT_FOUND)
-#
-#     def put(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         serializer = SnippetSerializer(snippet, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         snippet.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 # 👇 제네릭 클래스 기반 뷰
 class SnippetList(generics.ListCreateAPIView):
@@ -78,8 +12,31 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        """
+        인스턴스를 저장하는 과정을 조정하며, 요청이나 요청URL에서 정보를 가져와 원하는 대로 다룰 수 있다.
+        create() 메서드는 검증한 요청 데이터에 더하여 'owner' 필드도 전달한다.
+        :param serializer:
+        :return:
+        """
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
