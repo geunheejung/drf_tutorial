70aad8810a698aa0b2390dd5d8db295a04b41904
geunheejung <geuni1013@gmail.com>
2019-01-16 00:35:28 +0900

    5강 관게 & 하이퍼링크API 클리어

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,27 +4,20 @@ from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-    # source 인자로는 특정 필드를 지정 가능. 직렬화된 인스턴스의 속성 뿐만 아니라 마침표 표기 방식을 통해 특정 속성 탐색도 가능
-    # ReadOnlyField는 직렬화에 사용되었을 땐 언제나 읽기 전용
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = (
-            'id',
-            'title',
-            'code',
-            'linenos',
-            'language',
-            'style',
-            'owner'
-        )
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'username', 'snippets')
+
